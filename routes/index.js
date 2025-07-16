2aea58085c15392ee9f53591c418af6837ff8e58
Monet <codem0n3t@gmail.com>
2025-07-16 00:42:50 -0400

    corrected for testing

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,10 @@ const { login, createUser } = require("../controllers/users");
 
 router.post("/signup", createUser);
 router.post("/signin", login);
-router.use("/items", itemRoutes);
+
 router.use("/users", userRoutes);
+
 router.use(auth);
+router.use("/items", itemRoutes);
 
 module.exports = router;
