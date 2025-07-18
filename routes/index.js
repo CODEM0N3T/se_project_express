8226524d540dd0e7de8284bc5cd50f0cc8f3e9dd
Monet <codem0n3t@gmail.com>
2025-07-17 22:39:19 -0400

    Errors corrected

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const auth = require("../middlewares/auth");
 const userRoutes = require("./users");
 const itemRoutes = require("./clothingItems");
 const { login, createUser } = require("../controllers/users");
+const { getItems } = require("../controllers/clothingItems");
 
 router.post("/signup", createUser);
 router.post("/signin", login);
 router.use("/items", itemRoutes);
+router.get("/items", getItems);
 
 router.use(auth);
 router.use("/users", userRoutes);
