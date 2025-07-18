e51b16b8a069005b5dc8a5062f38ef02b44bf166
Monet <codem0n3t@gmail.com>
2025-07-17 22:27:22 -0400

    Errors corrected

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const { JWT_SECRET = "dev-secret" } = process.env;
+const { JWT_SECRET } = require("../utils/config");
 const { UNAUTHORIZED } = require("../utils/errors");
 
 module.exports = (req, res, next) => {
