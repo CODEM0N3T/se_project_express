4644658d90823dcf4129642c478071bc0b1d2a83
Monet <codem0n3t@gmail.com>
2025-07-20 22:28:07 -0400

    errors

--- app.js
+++ app.js
@@ -11,13 +11,6 @@ const { PORT = 3001 } = process.env;
 app.use(cors());
 app.use(express.json());
 
-if (process.env.NODE_ENV === "test") {
-  app.use((req, res, next) => {
-    req.user = { _id: "5d8b8592978f8bd833ca8133" };
-    next();
-  });
-}
-
 // Connect to MongoDB
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
