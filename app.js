8b4cd570ab958b3076ceadd0a7bcee2b240eb9e0
Monet <codem0n3t@gmail.com>
2025-08-24 15:25:31 -0400

    Project 15

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -24,6 +24,12 @@ mongoose
   .catch(console.error);
 
 app.use(requestLogger);
+
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
 
 app.use("/", mainRouter);
 
